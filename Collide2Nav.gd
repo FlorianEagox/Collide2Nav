f0e8c4cdbca5b1bb24119898a49992ca703b490d
Seth Painter <timytomy11@gmail.com>
2020-02-15 05:07:56 -0600

    When the button is pushed, it will find all staticbodies in the current scene

--- Collide2Nav.gd
+++ Collide2Nav.gd
@@ -6,6 +6,7 @@ var btnGenerate
 #var nav_toolbar_path = "/root/EditorNode/@@585/@@586/@@594/@@596/@@600/@@604/@@605/@@606/@@622/@@623/@@632/@@633/@@6278/@@6116/@@11825"
 #var nav_toolbar: HBoxContainer
 var editor_selection = get_editor_interface().get_selection()
+var selected_node: NavigationPolygonInstance
 
 func _enter_tree() -> void:
 	editor_selection.connect("selection_changed", self, "selection_changed") # Connect an event when the user selects a node in their scene tree
@@ -20,6 +21,7 @@ func selection_changed():
 	var nodes = editor_selection.get_selected_nodes() # Get the nodes they selected
 	if nodes.size() == 1 and nodes[0] is NavigationPolygonInstance: # If they only selected one, and it's a mesh
 		if not btnGenerate.get_parent(): # if the button hasn't been added yet
+			selected_node = nodes[0]
 			add_control_to_container(CONTAINER_CANVAS_EDITOR_MENU, btnGenerate) # add it to the menu
 			# nav_toolbar.add_child(btnGenerate)
 	else:
@@ -38,6 +40,17 @@ func _exit_tree() -> void:
 
 func generate():
 	print("Generating Mesh...")
+	var bodies = [] # All of the static bodies in the scene
+	get_of_type(get_editor_interface().get_edited_scene_root(), StaticBody2D, bodies) # Get every static body in the curently edited scene
+	print(bodies)
+
+# This function will recurse through every child of root, and if the child is of type type, add it to the referenced found_objects array
+func get_of_type(root: Node, type, found_objects):
+	for node in root.get_children():
+		if node is type:
+			found_objects.append(root)
+		get_of_type(node, type, found_objects)
+
 
 # This function is very useful for parsing actual Editor elements
 func print_children_readable(child: Control, level: int = 0):
