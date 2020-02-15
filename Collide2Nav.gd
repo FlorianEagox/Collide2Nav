a32e63d723d431e620436695e649a05f58f044f8
Seth Painter <timytomy11@gmail.com>
2020-02-15 04:20:57 -0600

    Added a button that will show up when the user is editing a NavMeshPolygonInstance

--- Collide2Nav.gd
+++ Collide2Nav.gd
@@ -1,10 +1,50 @@
 tool
 extends EditorPlugin
 
+var btnGenerate
+# We could put the button directly into the Polygon tools, but it looks functionally idential and is much easier just to add it to the canvas's menu
+#var nav_toolbar_path = "/root/EditorNode/@@585/@@586/@@594/@@596/@@600/@@604/@@605/@@606/@@622/@@623/@@632/@@633/@@6278/@@6116/@@11825"
+#var nav_toolbar: HBoxContainer
+var editor_selection = get_editor_interface().get_selection()
 
 func _enter_tree() -> void:
-	print("UWU")
+	editor_selection.connect("selection_changed", self, "selection_changed") # Connect an event when the user selects a node in their scene tree
+	btnGenerate = ToolButton.new()
+	btnGenerate.text = "Generate from bodies"
+	btnGenerate.hint_tooltip = "Generate from bodies"
+	btnGenerate.name = "btnGenerate"
+	btnGenerate.connect("pressed", self, "generate")
+	#nav_toolbar = get_node(nav_toolbar_path)
 
+func selection_changed():
+	var nodes = editor_selection.get_selected_nodes() # Get the nodes they selected
+	if nodes.size() == 1 and nodes[0] is NavigationPolygonInstance: # If they only selected one, and it's a mesh
+		if not btnGenerate.get_parent(): # if the button hasn't been added yet
+			add_control_to_container(CONTAINER_CANVAS_EDITOR_MENU, btnGenerate) # add it to the menu
+			# nav_toolbar.add_child(btnGenerate)
+	else:
+		if btnGenerate.get_parent(): # If we've added it before
+			remove_control_from_container(CONTAINER_CANVAS_EDITOR_MENU, btnGenerate) # Remove the button when we're not editing a mesh
+
+
+func get_plugin_name() -> String:
+	return "Collide2Nav"
 
 func _exit_tree() -> void:
-	pass
+	if btnGenerate.get_parent(): #If we've added it before
+		remove_control_from_container(CONTAINER_CANVAS_EDITOR_MENU, btnGenerate)
+	btnGenerate.queue_free()
+	#nav_toolbar.remove_child(btnGenerate)
+
+func generate():
+	print("Generating Mesh...")
+
+# This function is very useful for parsing actual Editor elements
+func print_children_readable(child: Control, level: int = 0):
+	if child and child.visible:
+		var tabs = ""
+		for i in range(level):
+			tabs += "\t"
+		print(tabs, child, child.get_path(), child.get_tooltip() if child.get_tooltip() else "")
+		for sub_child in child.get_children():
+			print_children_readable(sub_child, level+1)
